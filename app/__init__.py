aab07a46c5d3dd9aab2c5055a7c6982c7224c722
Abasiakan Akang <goddyjasper116@gmail.com>
2026-09-26 00:03:55 +0100

    feat: initialize Flask backend

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+
+    CORS(app)
+
+    from app.api.health import health_bp
+    app.register_blueprint(health_bp, url_prefix="/api")
+
+    return app
